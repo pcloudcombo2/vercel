3b8b226aaceca83132087a62ce8fbc7bc3843a59
Muhammad Aman <maman0022@outlook.com>
2023-04-18 00:18:25 -0500

    update

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 import axios from 'axios';
 import cors from 'cors';
 import got from 'cloudflare-scraper';
+import Xvfb from 'xvfb';
+const xvfb = new Xvfb();
 
 app.listen(process.env.PORT);
 app.use(cors());
@@ -12,9 +14,11 @@ app.get('/', main);
 
 async function main(req, res) {
   try {
+    xvfb.startSync();
     const response = await got.get('https://nowsecure.nl');
     console.log(response);
     res.status(200).send(response.body);
+    xvfb.stopSync();
   } catch (error) {
     console.log(error);
     res.status(400).send(error);
